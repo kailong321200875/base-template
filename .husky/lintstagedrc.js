8028ca263f69f01492f36a45a36d9213cdf95911
kailong321200875 <321200875@qq.com>
2022-06-11 17:26:54 +0800

    feat: add husky and commitlint

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['prettier --write'],
   // '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['prettier --write'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
   'package.json': ['prettier --write'],
   // '*.vue': ['prettier --write', 'stylelint --fix'],
