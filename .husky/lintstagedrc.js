2224ded3dd18b245f3ae2b04b428b24c5609818e
kailong321200875 <321200875@qq.com>
2022-06-11 17:19:00 +0800

    test

---
+++ .husky/lintstagedrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['prettier --write'],
+  // '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
+  'package.json': ['prettier --write'],
+  // '*.vue': ['prettier --write', 'stylelint --fix'],
+  '*.vue': ['prettier --write'],
+  // '*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write'],
+  '*.{scss,less,styl,css,html}': ['prettier --write'],
+  '*.md': ['prettier --write'],
+  '*.hbs': ['prettier --write']
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # base-template
-A basic template, including husky, commitlint, eslint, stylelint, prettier
+A basic template, including husky, commitlint, prettier
